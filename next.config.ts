4e45a6c162a522d1e06ab31ec2c822883bf90c59
Projector <231699516+Pr0jector23@users.noreply.github.com>
2025-10-27 17:53:29 +0400

    fixed type issues

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = { // i added the options
   output: 'export',
   images: { unoptimized: true }, // optional: disables Image Optimization
-  basePath: '/game-of-life>', // required for GitHub Pages
+  basePath: '/game-of-life', // required for GitHub Pages
   assetPrefix: '/game-of-life/', // required too
 };
 
