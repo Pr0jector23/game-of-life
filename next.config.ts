31fb30d0ad458485b1f7f422980a21b4905a0724
Projector <231699516+Pr0jector23@users.noreply.github.com>
2025-10-27 17:17:54 +0400

    setting up live demo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = { // i added the options
+  output: 'export',
+  images: { unoptimized: true }, // optional: disables Image Optimization
+  basePath: '/<your-repo-name>', // 👈 required for GitHub Pages
+  assetPrefix: '/<your-repo-name>/', // 👈 required too
 };
+
+module.exports = nextConfig; // i added this too
 
 export default nextConfig;
