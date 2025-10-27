49fd9a4218dec59fbcafacedbbf44e1205dbcc68
Projector <231699516+Pr0jector23@users.noreply.github.com>
2025-10-27 17:23:52 +0400

    minor fixes

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = { // i added the options
   output: 'export',
   images: { unoptimized: true }, // optional: disables Image Optimization
-  basePath: '/<your-repo-name>', // 👈 required for GitHub Pages
-  assetPrefix: '/<your-repo-name>/', // 👈 required too
+  basePath: '/game-of-life>', // required for GitHub Pages
+  assetPrefix: '/game-of-life/', // required too
 };
 
 module.exports = nextConfig; // i added this too
